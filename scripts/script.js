1a83dff39706972a42c3e3831d1795572c410d14
John Skoog <john.skoog.92@gmail.com>
2025-02-03 09:54:45 +0100

    added function playPauseMusic

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 const log = (msg) => console.log(msg);
 
 // I denna fil skriver ni all er kod
+const audio = new Audio('assets/pokemon_vs_trainer.mp3')
+function playPauseMusic () {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.paused;
+    }
+}
