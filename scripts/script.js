fecdad7f2806392a3b99e35a4dee51f8afaa578f
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-02-03 10:18:06 +0100

    KAN60-eventListenerForm

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,15 @@
 const log = (msg) => console.log(msg);
 
 // I denna fil skriver ni all er kod
+
+
+
+
+
+
+//Eventlistener submit form
+document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    //validateForm(formData.get('username'), formData.get('age'), formData.get('gender'))
+})
